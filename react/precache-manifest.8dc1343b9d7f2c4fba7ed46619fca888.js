2698faf7b3e0dde1f9c0d6c81ab95fbc982d274a
IzvekovWeb <izvekov.web@yandex.ru>
2021-03-11 13:59:18 +0300

    lab-4-react

---
+++ react/precache-manifest.8dc1343b9d7f2c4fba7ed46619fca888.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413fe89d5cbb06603a505a7215a8938e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edbabf547d90ec8d25a6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c35cecbdde4af717df9",
+    "url": "/static/js/2.049e0a35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.049e0a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbabf547d90ec8d25a6",
+    "url": "/static/js/main.5f4f399d.chunk.js"
+  },
+  {
+    "revision": "4390a351b0f90a7c34c6",
+    "url": "/static/js/runtime-main.8bc1365d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
